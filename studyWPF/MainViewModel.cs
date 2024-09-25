24e6f32302847fa61549bcb4090f73174da749e7
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-09-25 13:10:04 +0900

    feat: datatemplate에 대해서 학습 중, 추후 정리 예정

---
+++ studyWPF/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    class MainViewModel : ViewModelBase
+    {
+        private List<Student> students;
+
+        public MainViewModel()
+        {
+            students = Student.Students;
+        }
+
+        public List<Student> Students { get => students; set { students = value; NotifyPropertyChanged(); } }
+    }
+}
---
+++ studyWPF/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    class Student
+    {
+        public string Name { get; set; } = "";
+        public string Gender { get; set; } = "";
+        public string BirthDay { get; set; } = "";
+        public string School { get; set; } = "";
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Gender: {Gender}, Birthday: {BirthDay}, School: {School}";
+        }
+
+        public static List<Student> Students = new List<Student>()
+        {
+            new  Student{Name ="1", Gender="남", BirthDay="a", School="A"},
+            new  Student{Name ="2", Gender="남", BirthDay="b", School="B"},
+            new  Student{Name ="3", Gender="여", BirthDay="c", School="C"},
+            new  Student{Name ="4", Gender="남", BirthDay="d", School="D"},
+            new  Student{Name ="5", Gender="남", BirthDay="e", School="E"},
+            new  Student{Name ="6", Gender="여", BirthDay="f", School="F"},
+        };
+    }
+}
