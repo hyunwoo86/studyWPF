caba03f2a68559cc2e1b159f30b8aac3e6645493
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-09-26 09:22:43 +0900

    study: 개인적으로 button command에 대해서 학습을 하여 적용 하였다.

--- studyWPF/MainViewModel.cs
+++ studyWPF/MainViewModel.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace studyWPF
 {
     class MainViewModel : ViewModelBase
     {
         private List<Student> students;
+        public ICommand MyButtonCommand { get; }
 
         public MainViewModel()
         {
             students = Student.Students;
             CurrentViewModel = new CurrentViewModel();
+            MyButtonCommand = new RelayCommand(ChangeColorEvent);
+
         }
 
         public List<Student> Students { get => students; set { students = value; NotifyPropertyChanged(); } }
         public ViewModelBase CurrentViewModel { get; set; }
+
+        private void ChangeColorEvent(object parameter)
+        {
+            System.Windows.MessageBox.Show("Button Clicked!");
+        }
     }
 }
--- studyWPF/ViewModelBase.cs
+++ studyWPF/ViewModelBase.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace studyWPF
 {
@@ -25,6 +26,34 @@ namespace studyWPF
             member = value;
             NotifyPropertyChanged(propertyName);
             return true;
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
