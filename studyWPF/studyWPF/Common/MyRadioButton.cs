380b5bed062c9c21bcb4eb813b18ac2e97592174
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-11-26 08:42:32 +0900

    study: visual studio 2017은 wpf에서 usercontrol 사용 가능한지 확인이 필요 함.
    https://www.youtube.com/watch?v=WWvYYxamemY&list=PLlrfTSXS0LLLVJM9KlOeRC1IlPmQzos5x&index=12
    
    https://www.inflearn.com/community/questions/1208976/visual-studio-2017%EC%97%90%EC%84%9C%EB%8A%94-wpf-ui%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%A0-%EC%88%98-%EC%97%86%EB%82%98%EC%9A%94?srsltid=AfmBOopfoiJAlrVvR61dYfecjlflEUbPDhkp4vNQI19cG_q1hFrYA5jn

---
+++ studyWPF/studyWPF/Common/MyRadioButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace studyWPF.Common
+{
+    public class MyRadioButton : RadioButton
+    {
+        static MyRadioButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyRadioButton), new FrameworkPropertyMetadata(typeof(MyRadioButton)));
+        }
+    }
+}
--- studyWPF/studyWPF/MainViewModel.cs
+++ studyWPF/studyWPF/MainViewModel.cs
@@ -8,21 +8,5 @@ namespace studyWPF
 {
     class MainViewModel : ViewModelBase
     {
-        private List<Student> students = default;
-
-        public MainViewModel()
-        {
-            Students = Student.Students;
-        }
-
-        public List<Student> Students 
-        {
-            get => students;
-            set
-            {
-                students = value;
-                OnPropertyChanged();
-            }
-        }
     }
 }
