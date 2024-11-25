fd6af52924742ef1d198fec702f3a82af8cc67cf
hyunwoo86 <junghw86@gmail.com>
2024-11-25 23:42:27 +0900

    git: ControlTemplate에 대해서 확인 하였다.

---
+++ studyWPF/studyWPF/Models/ComboItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace studyWPF.Models
+{
+    class ComboItem
+    {
+        public string Value { set; get; } = default;
+        public Brush Brush { set; get; } = default;
+
+        public override string ToString()
+        {
+            return $"Value: {Value}, Brush: {Brush}";
+        }
+
+        public static List<ComboItem> Items = new List<ComboItem>()
+        {
+            new ComboItem{Value="빨간색", Brush = Brushes.Red},
+            new ComboItem{Value="파란색", Brush = Brushes.Blue},
+            new ComboItem{Value="초록색", Brush = Brushes.Green},
+
+        };
+    }
+}
---
+++ studyWPF/studyWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using studyWPF.Models;
+using System.Collections.Generic;
+
+namespace studyWPF.ViewModels
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Items = ComboItem.Items;
+        }
+
+        public List<ComboItem> Items { get; set; }
+    }
+}
