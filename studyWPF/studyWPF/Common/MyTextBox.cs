099ba02dc564c84c81fa0af2cd1596db94a11e10
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-11-26 14:54:52 +0900

    chor: generic study를 위해서 임의로 하나 더 넣어 봣음

---
+++ studyWPF/studyWPF/Common/MyTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace studyWPF.Common
+{
+    public class MyTextBox : TextBox
+    {
+        static MyTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(MyTextBox),
+                new FrameworkPropertyMetadata(typeof(MyTextBox))
+                );
+        }
+    }
+}
+
