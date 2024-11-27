17b75d6bd5d059cc9206ee74de4ed1d3acaf8bf9
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-11-28 08:25:36 +0900

    fix: usercontrol에서 마든 thumbnail을 main에 listview에 적용 및 표시

---
+++ studyGN/studyGN/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using studyGN.Common;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace studyGN
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        // ObservableCollection을 사용하여 동적으로 리스트를 관리
+        public ObservableCollection<Thumbnail> Items { get; set; }
+
+        // INotifyPropertyChanged 이벤트 구현
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            // 초기 데이터 설정
+            Items = new ObservableCollection<Thumbnail>
+            {
+                new Thumbnail(),
+                new Thumbnail(),
+                new Thumbnail(),
+                new Thumbnail(),
+                new Thumbnail(),
+                new Thumbnail(),
+                new Thumbnail(),
+            };
+        }
+
+        // PropertyChanged 이벤트를 호출하는 메서드
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
