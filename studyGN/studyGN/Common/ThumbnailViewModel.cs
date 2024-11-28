fc6f8f6e33e316e13a7aa4cd727cde38c6ce47d2
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-11-28 14:45:00 +0900

    chor: 테두리 색 빨간색으로 하지 않고 보라색으로 변경

--- studyGN/studyGN/Common/ThumbnailViewModel.cs
+++ studyGN/studyGN/Common/ThumbnailViewModel.cs
@@ -4,13 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace studyGN.Common
 {
     class ThumbnailViewModel : NotifiyChangedBase
     {
-
         private string _operationType;
+        private string _positionType;
+        private bool _isSelected;
+        public ICommand SelectCommand { get; }
+
+        public ThumbnailViewModel(string operationType, string positionType, bool isSelected = false)
+        {
+            _operationType = operationType;
+            _positionType = positionType;
+            _isSelected = isSelected;
+            SelectCommand = new RelayCommand(OnSelect);
+        }
 
         public string OperationType
         {
@@ -22,20 +33,39 @@ namespace studyGN.Common
             }
         }
 
-        private string _positionType;
-
         public string PositionType
         {
             get { return _positionType; }
             set { _positionType = value; OnPropertyChanged(); }
         }
 
-
-
-        public ThumbnailViewModel(string operationType, string positionType)
+        public bool IsSelected
         {
-            _operationType = operationType;
-            _positionType = positionType;
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private void OnSelect(object parameter)
+        {
+            if (parameter is ThumbnailViewModel selectedItem)
+            {
+                //// 외부에서 관리하는 로직에 따라 다른 항목을 선택 해제
+                //foreach (var item in _parentCollection)
+                //{
+                //    item.IsSelected = false;
+                //}
+
+                // 현재 항목만 선택
+                IsSelected = true;
+            }
         }
     }
 }
