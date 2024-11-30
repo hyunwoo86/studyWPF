c9681a377d4c59a0c22801ce0d70ea1346483fa6
hyunwoo86 <junghw86@gmail.com>
2024-11-30 23:49:55 +0900

    study: - "IValueConverter"를 이용한 Convert 구현 학습 - 강의에선 UserControl에서 [ObservableObject] 에 관한 Attribute가 수행이 되는데,    나는 안된다, 이에 관련 질문이 있었고 찾아보니, UserControl에서는 이미 상속을 받았기 때문이다.    주로 해당 Attribute는 ViewModel에서 사용 한다.

--- KabulTalk/KabulTalk/Controls/TitleBar.xaml.cs
+++ KabulTalk/KabulTalk/Controls/TitleBar.xaml.cs
@@ -1,28 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WpfLib.Extensions;
 
 namespace KabulTalk.Controls
 {
     /// <summary>
     /// TitleBar.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class TitleBar : UserControl
+
+    public partial class TitleBar : UserControl, INotifyPropertyChanged
     {
+        private Window _parentWindow;
+        private WindowState _winState;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public WindowState WinState
+        {
+            get { return _winState; }
+            set 
+            {
+                _winState = value;
+                OnPropertyChanged(nameof(WinState));
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Window ParentWindow
+        {
+            set { _parentWindow = value; }
+            get
+            {
+                if (_parentWindow == null)
+                {
+                    _parentWindow = this.FindParent<Window>();
+                }
+                return _parentWindow;
+            }
+
+        }
+
         public TitleBar()
         {
             InitializeComponent();
+
+            btnExit.Click += BtnExit_Click;
+            btnMaximize.Click += BtnMaximize_Click;
+            btnMinimize.Click += BtnMinimize_Click;
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            //// 이렇게 사용하면 매번 재귀함수로 호출되어서 비효율
+            //btnMinimize.FindParent<Window>().WindowState = WindowState.Minimized;
+
+            WinState = WindowState.Minimized;
+            ParentWindow.WindowState = WinState;
+        }
+
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            WinState = ParentWindow.WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+
+            ParentWindow.WindowState = WinState;
+
+            //btnMinimize.FindParent<Window>().WindowState = WindowState.Maximized;
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            //btnMinimize.FindParent<Window>().Close();
+            ParentWindow.Close();
         }
     }
 }
---
+++ KabulTalk/KabulTalk/Converters/WindowStateMarginConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KabulTalk.Converters
+{
+    public class WindowStateMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            WindowState state = (WindowState)value;
+            if (state == WindowState.Normal)
+            {
+                return new Thickness(0);
+            }
+            else
+            {
+                var param = (string)parameter;
+                var right = param == "Exit" ? 7 : 0;
+                return new Thickness(0, 7, 0, 0);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
