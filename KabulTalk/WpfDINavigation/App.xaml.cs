e5cc3534a72523908294ed623264d0d0cec12981
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-12-03 08:57:47 +0900

    study: Dependency Injection(DI) IoC 를 이용한 Navigation
    프로젝트를 구성하기 위한 기초 단계
    10:55
    
    https://www.youtube.com/watch?v=KWZ3bxBe1mQ&list=PLlrfTSXS0LLKHfOfwM31jJw3SHuDnkF49&index=12

---
+++ KabulTalk/WpfDINavigation/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+using WpfDINavigation.ViewModels;
+using WpfDINavigation.Views;
+
+namespace WpfDINavigation
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        public new static App Current => (App)Application.Current;
+
+        private IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // ViewModels
+            services.AddSingleton<MainViewModel>();
+
+            // View
+            services.AddSingleton(s => new MainView()
+            {
+                DataContext = s.GetRequiredService<MainViewModel>()
+            });
+
+            return services.BuildServiceProvider();
+        }
+
+        public App()
+        {
+            Services = ConfigureServices();
+
+            var mainView = Services.GetRequiredService<MainView>();
+            mainView.Show();
+        }
+
+        public IServiceProvider Services { get; }
+    }
+
+}
---
+++ KabulTalk/WpfDINavigation/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDINavigation.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private INotifyPropertyChanged _currentViewModel;
+
+        public MainViewModel()
+        {
+            CurrentViewModel = new LoginViewModel();
+        }
+
+
+        public INotifyPropertyChanged CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set { _currentViewModel = value; OnPropertyChanged(); }
+        }
+
+    }
+}
