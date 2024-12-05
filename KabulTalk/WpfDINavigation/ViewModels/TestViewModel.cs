149e753b84b5dd4569b6a25998184d8df0771109
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-12-05 13:06:21 +0900

    feat: styles를 만들어 button에 대한 스타일을 전역으로 만들어서 적용 해보았다.

--- KabulTalk/WpfDINavigation/ViewModels/TestViewModel.cs
+++ KabulTalk/WpfDINavigation/ViewModels/TestViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
+using WpfDINavigation.Commands;
+using WpfDINavigation.Services;
 
 namespace WpfDINavigation.ViewModels
 {
     public class TestViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+
+        public ICommand ToSignupCommand { get; set; }
+        public ICommand ToTestCommand { get; set; }
+
+        public TestViewModel(INavigationService navigationService)
+        {
+            ToSignupCommand = new RelayCommand<object>(ToSignup);
+            ToTestCommand = new RelayCommand<object>(ToTest);
+            _navigationService = navigationService;
+        }
+
+        private void ToSignup(object _)
+        {
+            _navigationService.Navigate(NaviType.SignView);
+        }
+
+        private void ToTest(object _)
+        {
+            _navigationService.Navigate(NaviType.TestView);
+        }
     }
 }
