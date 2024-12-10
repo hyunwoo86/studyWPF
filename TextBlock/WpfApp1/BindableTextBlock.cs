06c911efa8cfb53064a385fdeb51f9b960a5d92c
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-12-11 08:24:19 +0900

    [STUDY] VENUS 초기 화면에서 상태 표시에 대해서 TEXTBLOCK을 통해서 구현 예정 TEXTBLOCK을 이용하면 각 문장에 대해서 색 및 굵기 등 다양하게 표시 할 수가 있어서 선택함

---
+++ TextBlock/WpfApp1/BindableTextBlock.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WpfApp1
+{
+    public class BindableTextBlock : TextBlock
+    {
+        public ObservableCollection<Inline> BindableInlineList
+        {
+            get { return (ObservableCollection<Inline>)GetValue(BindableInlineListProperty); }
+            set { SetValue(BindableInlineListProperty, value); }
+        }
+
+        public static readonly DependencyProperty BindableInlineListProperty =
+            DependencyProperty.Register(
+                "BindableInlineList",
+                typeof(ObservableCollection<Inline>),
+                typeof(BindableTextBlock),
+                new UIPropertyMetadata(new ObservableCollection<Inline>(), OnPropertyChanged));
+
+        //private static void OnPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        //{
+        //    BindableTextBlock textBlock = sender as BindableTextBlock;
+        //    if (textBlock != null)
+        //    {
+        //        if (e.OldValue is ObservableCollection<Inline> oldList)
+        //        {
+        //            oldList.CollectionChanged -= textBlock.InlineCollectionChanged;
+        //        }
+
+        //        if (e.NewValue is ObservableCollection<Inline> newList)
+        //        {
+        //            newList.CollectionChanged += textBlock.InlineCollectionChanged;
+        //        }
+        //    }
+        //}
+
+        private static void OnPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var textBlock = sender as BindableTextBlock;
+            if (textBlock == null)
+                return;
+
+            // 이전 리스트의 이벤트 해제
+            if (e.OldValue is ObservableCollection<Inline> oldList)
+            {
+                oldList.CollectionChanged -= textBlock.InlineCollectionChanged;
+            }
+
+            // 새 리스트의 이벤트 연결
+            if (e.NewValue is ObservableCollection<Inline> newList)
+            {
+                newList.CollectionChanged += textBlock.InlineCollectionChanged;
+                // 기존 아이템 초기화 처리
+                foreach (var inline in newList)
+                {
+                    if (inline != null)
+                    {
+                        textBlock.Inlines.Add(inline);
+                    }
+                }
+            }
+        }
+
+
+        private void InlineCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (Inline inline in e.NewItems)
+                {
+                    if (inline != null)
+                    {
+                        this.Inlines.Add(inline);
+                    }
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (Inline inline in e.OldItems)
+                {
+                    if (inline != null)
+                    {
+                        this.Inlines.Remove(inline);
+                    }
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                this.Inlines.Clear();
+            }
+        }
+    }
+}
---
+++ TextBlock/WpfApp1/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Inline> _bindableInlineList;
+
+        public ObservableCollection<Inline> BindableInlineList
+        {
+            get { return _bindableInlineList; }
+            set
+            {
+                if (_bindableInlineList != value)
+                {
+                    _bindableInlineList = value;
+                    OnPropertyChanged(nameof(BindableInlineList));
+                }
+            }
+        }
+
+        public ViewModel()
+        {
+            BindableInlineList = new ObservableCollection<Inline>
+            {
+                new Run("Hello ") { Foreground = Brushes.Black },
+                new Run("World!") { Foreground = Brushes.Red, FontSize = 22, }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
