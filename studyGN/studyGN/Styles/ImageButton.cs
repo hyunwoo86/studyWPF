a59d8d5d6eb69e3edb6908c0f3d130539772ec07
DESKTOP-4DNUL0B\User <hwjung@genoray.com>
2024-12-16 08:56:48 +0900

    [STUDY] 버튼에 이미지 옆에 넣는거 확인

---
+++ studyGN/studyGN/Styles/ImageButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace studyGN.Styles
+{
+    public class ImageButton : Button
+    {
+        public static readonly DependencyProperty ImagePathProperty =   DependencyProperty.Register(
+                                                                        nameof(ImagePath),
+                                                                        typeof(string),
+                                                                        typeof(ImageButton),
+                                                                        new PropertyMetadata(default(string)));
+
+        public string ImagePath
+        {
+            get => (string)GetValue(ImagePathProperty);
+            set => SetValue(ImagePathProperty, value);
+        }
+    }
+}
